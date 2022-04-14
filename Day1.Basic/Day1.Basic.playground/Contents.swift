cbe68f10de2e12d371cf7fb5208e43862d4cf88e
dofuru0621 <74231280+dofuru0621@users.noreply.github.com>
2022-04-14 19:35:42 +0800

    Add files via upload

---
+++ Day1.Basic/Day1.Basic.playground/Contents.swift
@@ -0,0 +1,40 @@
+var helloMessage: String = "Hello, Swfit!"
+
+let myScore: Int = 100
+
+let 最大長度 = 10
+var 當前長度 = 0
+
+var game = "LOL", playage = 11, cost = 200
+
+var helloMessage: String
+helloMessage = "歡迎光臨“
+
+var 國文分數, 數學分數, 理化分數: Int
+var 姓名: String, 年齡: Int, 身高, 體重: Double
+
+let 最大長度 = 10
+
+//123木頭人
+
+/*
+今天天氣好好
+午餐想吃鍋燒意麵
+晚餐想吃烤香腸
+*/
+
+/*
+多行註解裡又包著一個註解
+
+/*
+大人啊
+可以這樣包了又包的嗎
+*/
+
+答案是可以的
+臨時要註解一大串程式碼，這個就很好用。
+*/
+
+let message = "加分好不影響喔！";
+
+let message = "加分好不影響喔！"; print(message)
